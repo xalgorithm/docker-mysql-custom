6efec45297fd42a5f22d26f19164932843a5ed53
Vincent Flesouras <x.algorithm@gmail.com>
2016-08-15 21:22:48 -0700

    Update mysql_docker_run.sh

--- mysql_docker_run.sh
+++ mysql_docker_run.sh
@@ -3,6 +3,6 @@ docker run --name xalg-mysql \
   -v ~/docker-bin/mysql-docker/conf:/etc/mysql/conf.d \
   -v ~/docker-bin/dumps:/docker-entrypoint-initdb.d \
   --volumes-from mysql_data \
-  -e MYSQL_ROOT_PASSWORD='K3yturtl3clock!' \
+  -e MYSQL_ROOT_PASSWORD='' \
   --env-file=env_file \
   -d mysql:5.6
